0816f43c9768b0a7005a516dca3fed051861221e
lib <lib.martinito@pm.me>
2022-03-21 12:51:05 +0800

    Added and refined the target selection box

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function Header({ avatarNames, avatarImg }) {
   return (
     <div className="header">
-      <h1 className="header__title">Where's Waldo</h1>
+      <h1 className="header__title">Where&apos;s Waldo</h1>
       <div className="header__avatars">
         {avatarNames.map((el) => (
           <div className="avatar">
@@ -17,31 +17,64 @@ function Header({ avatarNames, avatarImg }) {
   );
 }
 
-const Target = () => {
-  return <div className="target"></div>;
-};
+function Target({ location }) {
+  return (
+    <div
+      className="target"
+      style={{
+        position: "absolute",
+        left: `${location.x}px`,
+        top: `${location.y}px`,
+      }}
+    />
+  );
+}
 
-const SelectionMenu = ({ avatarNames, avatarImg }) => {
+function SelectionMenu({ avatarNames, avatarImg }) {
   return (
     <div className="selection">
       {avatarNames.map((el) => (
-        <div className="avatar">
-          <img src={avatarImg[el]} alt={el} className="avatar__img" />
-          <div className="avatar__name">{el}</div>
+        <div className="avatar avatar--selection">
+          <img
+            src={avatarImg[el]}
+            alt={el}
+            className="avatar__img avatar__img--selection"
+          />
+          <div className="avatar__name avatar__name--selection">{el}</div>
         </div>
       ))}
     </div>
   );
-};
+}
 
-const Body = ({ avatarNames, avatarImg }) => {
+function Body({ avatarNames, avatarImg }) {
+  const [targetLocation, setTargetLocation] = useState({
+    x: 0,
+    y: 0,
+  });
+  const updateCoordinates = (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    const element = e.currentTarget.getBoundingClientRect();
+    const newX = x - element.left - 30;
+    const newY = y - element.top - 30;
+    setTargetLocation({ x: newX, y: newY });
+  };
   return (
     <div className="body">
-      <Target />
-      <SelectionMenu avatarNames={avatarNames} avatarImg={avatarImg} />
+      <div className="body__wrapper">
+        <div
+          className="body__img"
+          onClick={updateCoordinates}
+          aria-hidden="true"
+        >
+          <Target location={targetLocation} />
+          <SelectionMenu avatarNames={avatarNames} avatarImg={avatarImg} />
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 function App() {
   const [avatarNames] = useState(["Waldo", "Odlaw", "Wenda"]);
@@ -51,24 +84,10 @@ function App() {
     Wenda: "./images/wenda.webp",
   });
 
-  const getCoordinates = (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    const element = e.currentTarget.getBoundingClientRect();
-    console.log(mouseX);
-    console.log(mouseY);
-    console.log(mouseX - element.left);
-    console.log(mouseY - element.top);
-  };
-
   return (
     <div className="container">
       <Header avatarNames={avatarNames} avatarImg={avatarImg} />
-      <Body
-        avatarNames={avatarNames}
-        avatarImg={avatarImg}
-        getCoordinates={getCoordinates}
-      />
+      <Body avatarNames={avatarNames} avatarImg={avatarImg} />
     </div>
   );
 }
